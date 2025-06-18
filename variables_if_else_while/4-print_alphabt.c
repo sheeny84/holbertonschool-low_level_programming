0570fd524dc93dc403af9f62d458f4e8220980df
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-18 02:47:43 +0000

    Add solution for task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		if (letter!='e' && letter!='q')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
