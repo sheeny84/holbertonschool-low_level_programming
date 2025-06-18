3ed3de0c89a91074d774797fe01ed8306f933207
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-18 08:39:33 +0000

    Update for betty style and remove extra char

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
 int main(void)
 {
 	int n;
 	int strLength;
 	char str[] = "_putchar";
-	
+
 	strLength = 8;
-	while(n <= strLength) 
+	while (n < strLength)
 	{
 		_putchar(str[n]);
 		n++;
