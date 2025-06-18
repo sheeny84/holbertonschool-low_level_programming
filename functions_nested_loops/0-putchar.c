a3b8f143b1147777d73c386e7105f06d9cd9d5b1
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-18 08:34:33 +0000

    Add solution for task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
-	printf("_putchar\n");
+	int n;
+	int strLength;
+	char str[] = "_putchar";
+	
+	strLength = 8;
+	while(n <= strLength) 
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
 	return (0);
 }
