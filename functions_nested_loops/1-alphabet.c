750b95288f05d85a67ccabc22b62d7fb62b62237
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-18 09:00:47 +0000

    Add files for task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes the alphabet in lowercase using _putchar
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int n = 'a';
+	
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
