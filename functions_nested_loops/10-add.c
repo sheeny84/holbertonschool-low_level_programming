5a3dc0c3a94f738df1ef594c063b7e15eb730248
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-20 04:37:53 +0000

    Add files for task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers together
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: result of the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
