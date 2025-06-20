0ecb5808a65bf1a2a552d7406a3cfbfb693bdc89
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-20 00:53:50 +0000

    Add files for task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: the integer to perform the computation on
+ *
+ * Return: the computed absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1*n);
+}
