8b5dcbfa2474dd81b2916f9cc122d593a72b1dbf
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-24 01:30:00 +0000

    Add files for task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value pointed to by the provided pointer to 98
+ * @n: pointer to an int
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains the task files for the 'C - Pointers, arrays and strings' project.
