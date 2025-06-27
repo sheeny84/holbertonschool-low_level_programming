1ca9d20214558efc1fac8d64d95f399eed313ad2
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-27 02:41:31 +0000

    Fix betty style for task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,12 +13,12 @@ char *leet(char *str)
 	int i = 0, j = 0;
 	char str1[11] = "aAeEoOtTlL";
 	char str2[11] = "4433007711";
-
 	/* go through each char in the string */
 	while (str[i] != '\0')
 	{
 		/* go through str1 and check for matches */
-		while (str1[j] != '\0') {
+		while (str1[j] != '\0')
+		{
 			if (str[i] == str1[j])
 				str[i] = str2[j];
 			j++;
