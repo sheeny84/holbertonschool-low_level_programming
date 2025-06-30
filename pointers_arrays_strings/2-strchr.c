4f55fca32ad4403a178fa0c8c2aaa830e4171391
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-30 01:54:23 +0000

    Add file for task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character in a string
+ * @s: pointer to the string
+ * @c: character to locate
+ *
+ * Return: pointer to first occurence of character in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int foundChar = 0;
+
+	while (*s != '\0' && foundChar != 1)
+	{
+		if (*s == c)
+			foundChar = 1;
+		else
+			s = s + 1;
+	}
+	return (s);
+}
