b4b630e90bfbc428b384f5a49605a225f3fb7731
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-30 01:39:10 +0000

    Add file for "C - Even more pointers, arrays, and strings" task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant byte 
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
