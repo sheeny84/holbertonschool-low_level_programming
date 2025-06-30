71b3e5438492d94c005530a3abda6ce131f3f908
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-30 02:03:05 +0000

    Add condition to account for c = NULL case for task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,8 @@
  * @s: pointer to the string
  * @c: character to locate
  *
- * Return: pointer to first occurence of character in the string
+ * Return: pointer to first occurence of character in the string,
+ * NULL if the char is not found
  */
 
 char *_strchr(char *s, char c)
@@ -19,8 +20,9 @@ char *_strchr(char *s, char c)
 		else
 			s = s + 1;
 	}
-	if (*s == '\0')
-		return (0);
+	/* Account for the case where c is the null character */
+	if (*s == '\0' && c != '\0')
+		return (0); /* return null if the char is not found */
 	else
 		return (s);
 }
