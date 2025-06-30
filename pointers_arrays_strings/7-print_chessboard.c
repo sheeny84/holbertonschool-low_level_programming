c2d2c7371c45c4a51f26fbac8181019fa77712dc
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-06-30 08:30:08 +0000

    Add file for task 6 (note file number is to agree with checker)

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: pointer to the chessboard array
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+	
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+		j = 0;
+	}
+}
