bd7f448cb960c7344def35ae4ba279123ba78178
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-01 05:08:10 +0000

    Add files for task 0, header and read me

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion: print a string followed by a new line
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+This folder contains the task files for the 'C - Recursion' project.
