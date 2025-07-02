b688857d5ff97342b4d0732f1463a643b1abbf43
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-02 02:48:26 +0000

    Add files for task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all the arguments recieved
+ * @argc: count of arguments
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
