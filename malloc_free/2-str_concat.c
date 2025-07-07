d40aef27fcc98024feddb2d2a8fbb88202e2cce6
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-07 07:58:33 +0000

    Add handling for NULL strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,27 +17,30 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	/* determine the length of str1 and str2 */
-	while (s1[length1] != '\0' && s1 != NULL)
+	while (s1 != NULL && s1[length1] != '\0')
 	{
 		length1++;
 	}
-	while (s2[length2] != '\0' && s2 != NULL)
+	while (s2 != NULL && s2[length2] != '\0')
 	{
 		length2++;
 	}
-	length2++; /* account for null byte */
 	/* allocate memory using malloc */
 	ptr = malloc((length1 + length2) * sizeof(char));
 	/* copy string to memory providing it was successfully allocated */
-	while (i < length1 && ptr != NULL)
+	if (ptr == NULL)
+		return (NULL);
+	while (i < length1)
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
-	while (j < length2 && ptr != NULL)
+	while (j < length2)
 	{
 		ptr[i + j] = s2[j];
 		j++;
 	}
+	if (s1 != NULL && s2 != NULL)
+		ptr[i+j] = '\0';
 	return (ptr);
 }
