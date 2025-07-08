ce02f2906644d8de5c7daf720d2b0de835305c7c
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-08 03:49:14 +0000

    Add code to free memory if malloc fails

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,21 +14,32 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i = 0, j = 0;
+	int i = 0, j = 0, k = 0;
 
 	/* invalid width or height */
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	/* allocate memory for pointers to pointers */
 	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL) /* i.e. malloc failed */
 		return (NULL);
+
 	/* allocate memory for each row */
 	while (i < height)
 	{
 		ptr[i] = malloc(width * sizeof(int));
 		if (ptr[i] == NULL) /* i.e. malloc failed */
+		{
+			/* free memory before returning NULL */
+			while (k < i)
+			{
+				free(ptr[k]);
+				k++;
+			}
+			free(ptr);
 			return (NULL);
+		}
 		/* initialise memory to 0 */
 		while (j < width)
 		{
