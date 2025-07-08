4e8ba1e00b76ebdde69fde0338bcce7f42f9ab5a
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-08 07:10:18 +0000

    Add files for task 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_range - create an array of integers from min to max inclusive
+ * @min: starting value for the array
+ * @max: final value for the array
+ *
+ * Return: pointer to the allocated memory
+ * NULL on failure
+ */
+
+int *array_range(int min, int max)
+{
+	int i = 0, length;
+	int *ptr;
+
+	/* invalid input */
+	if (min < 0 || max < 0 || min > max)
+		return (NULL);
+
+	/* allocate memory */
+	length = max - min + 1;
+	ptr = malloc(length * sizeof(int));
+	while (i < length && ptr != NULL)
+	{
+		ptr[i] = min + i;
+		i++;
+	}
+	return (ptr);
+}
