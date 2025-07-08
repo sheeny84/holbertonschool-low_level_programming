859ad3b2227dcad5e538deee5970297332b4491f
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-08 03:40:12 +0000

    Add files for task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - initialise a two dimensional array (grid) of integers to 0
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to the 2 dimensional array of integers
+ * NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i = 0, j = 0;
+
+	/* invalid width or height */
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	/* allocate memory for pointers to pointers */
+	ptr = malloc(height * sizeof(int *));
+	/* allocate memory for each row */
+	while (i < height)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		/* initialise memory to 0 */
+		while (j < width)
+		{
+			ptr[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (ptr);
+}
