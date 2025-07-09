cb27eac7d3ccdb86541e827ff92d9712fb5b6bac
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-09 05:56:18 +0000

    Add files for task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: struct for dog type
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
