98b0ef16ecb9c07011ccf8edadb1c83608000b74
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-09 03:28:59 +0000

    Add struct documentation for task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - dog
+ * @name: pointer to name of the dog
+ * @age: age of the dog
+ * @owner: pointer to owner of the dog
+ *
+ * Description: struct for dog type
+ */
 struct dog
 {
 	char *name;
