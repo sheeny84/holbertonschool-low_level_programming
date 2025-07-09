d65a14c66fc417885add73c28166ceee9695ae87
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-09 03:53:22 +0000

    Add handling for null pointer to task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialise a variable of type struct dog
@@ -11,6 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	struct dog my_dog;
+
+	if (d == NULL)
+		d = &my_dog;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
