e53ee6f0b74d65ad5ef15de409306ce53bb7241c
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-16 06:42:29 +0000

    Add files for task 2 - print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings
+ * @separator: string to be printed between numbers
+ * @n: number of strings passed to the function
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *str;
+	va_list ap;
+
+	va_start(ap, n); /* start iterating through the list */
+	while (i < n)
+	{
+		str = va_arg(ap, char *); /* argument is of type char * */ 
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+					
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(ap); /* free the list */
+}
