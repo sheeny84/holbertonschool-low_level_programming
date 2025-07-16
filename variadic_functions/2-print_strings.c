0d53cba18479aa36a0d60e47b1e140dcf6ca198e
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-16 06:45:16 +0000

    Fix betty style

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,12 +18,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n); /* start iterating through the list */
 	while (i < n)
 	{
-		str = va_arg(ap, char *); /* argument is of type char * */ 
+		str = va_arg(ap, char *); /* arg is of type char pointer */
 		if (str != NULL)
 			printf("%s", str);
 		else
 			printf("(nil)");
-					
 		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 		i++;
