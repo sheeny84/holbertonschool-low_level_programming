0c869bc6c8c94dbae0eae37e64158d1886292de0
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-28 06:35:52 +0000

    Add file for task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * list_len - get the number of elements in the list
+ * @h: pointer to head node of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+	int nodes = 0; /* number of nodes */
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next; /* move to the next node */
+	}
+	return (nodes);
+}
