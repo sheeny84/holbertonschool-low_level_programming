ba79f3a966383aff6ee69d1b2324ff49d18cd323
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-07-29 01:31:07 +0000

    Add logic to point head to new node in task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,9 +18,30 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL) 
 		return (NULL);
 	
+	/* add data to new node */
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
-	
+
+	/* update head to point to new node */
+	*head = new_node;
+
 	return (new_node);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @str: string
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ void print_null(void);
 void print_len(unsigned int len);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+unsigned int _strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
