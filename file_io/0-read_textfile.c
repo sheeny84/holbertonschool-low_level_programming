483c3d21ee0e3f1fc64b5453821995cf4b2bd583
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-01 01:44:58 +0000

    Make buf very big

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,7 +16,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[1024]; /* buffer to read file into */
+	char buf[2000]; /* buffer to read file into */
 	int fd; /* file descriptor */
 	ssize_t bytes; /* number of bytes read/written */
 
