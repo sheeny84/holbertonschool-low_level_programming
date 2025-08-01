d8d0c74c63db819e8f7876a0f950c40789d31de1
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-01 02:46:01 +0000

    Fix flags and mode for write function in task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,8 +20,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* create file using open */
+	/* flags set to create file if needed and open in write only mode */
 	/* set permissions to rw------- if it does not exist */
-	fd = open(filename, O_CREAT, 600);
+	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
@@ -30,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		bytes++;
 	}
-
+	
 	/* write to file */
 	bytes = write(fd, text_content, bytes);
 	if (bytes == -1)
--- file_io/1-main.c
+++ file_io/1-main.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+#include <errno.h>
+#include <string.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main()
 {
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+    	int ret;
+	ret = create_file("_file_0", "Best School !");
+	printf("%d\n", ret);
+	printf("%s\n", strerror(errno));
+	return (0);
 }
