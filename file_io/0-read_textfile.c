58c394dbb740c65ebd1244dbfe7c7e3c4af1b926
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-01 01:40:51 +0000

    Remove mode so permissions are not overwritten

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	/* open the file in read only mode */
-	fd = open(filename, O_RDONLY, 0666);
+	fd = open(filename, O_RDONLY); /* file already exists so mode not reqd */
 	if (fd == -1)
 		return (0);
 
